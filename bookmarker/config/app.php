cfed6f2a82c097e22ff06069a11bd3c928f411ae
Arthur Streit <arthur.streit@oneweb.com.br>
2024-08-27 09:37:02 -0300

    Recuperando bookmarks com uma tag especifica

--- bookmarker/config/app.php
+++ bookmarker/config/app.php
@@ -80,7 +80,7 @@ return [
      *   You should treat it as extremely sensitive data.
      */
     'Security' => [
-        'salt' => env('SECURITY_SALT'),
+        'salt' => env('SECURITY_SALT', 'Security.salt'),
     ],
 
     /*
